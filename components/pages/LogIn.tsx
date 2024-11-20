29d5ef0a6eaa89d9d5fd2cf46ac40da4e286265a
JakieBoi77 <jakef7@icloud.com>
2024-11-20 00:21:54 -0500

    fix: login page text

--- components/pages/LogIn.tsx
+++ components/pages/LogIn.tsx
@@ -69,7 +69,7 @@ const LogIn = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 className="shadow-md border-2 px-3 py-2 rounded-md focus:outline-none"
                 type="password"
-                placeholder="Set a password"
+                placeholder="Enter your password"
                 required
               />
               <input
