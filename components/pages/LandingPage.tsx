ae370c70e7d9cfbe407c86ea293232c42740eb40
JakieBoi77 <jakef7@icloud.com>
2024-11-20 00:34:34 -0500

    fix: landing page border beam

--- components/pages/LandingPage.tsx
+++ components/pages/LandingPage.tsx
@@ -75,6 +75,7 @@ export default function Home() {
           </Link>
           <div className="relative mt-12 md:mt-[8rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,black_30%,transparent)]">
             <div className="rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,#9500ff,#9500ff,transparent_40%)] before:animate-image-glow">
+              <div className="absolute inset-[0] rounded-[inherit] [border:1.5px_solid_transparent] ![mask-clip:padding-box,border-box] ![mask-composite:intersect] [mask:linear-gradient(transparent,transparent),linear-gradient(white,white)] after:absolute after:aspect-square after:w-[200px] after:animate-border-beam after:[animation-delay:-11s] after:[background:linear-gradient(to_left,#9500ff,#fe8bbb,transparent)] after:[offset-anchor:90%_50%] after:[offset-path:rect(0_auto_auto_0_round_calc(200px))]" />
               <img src="/images/background-desktop.jpg" alt="Hero Image" className="relative w-full h-full rounded-[inherit] border-white/10 object-contain" />
             </div>
           </div>
